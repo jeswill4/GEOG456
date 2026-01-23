b00df05b2167e3cb3f4eefe37643c7668389a6fa
Jesse Williams <jeswill@unc.edu>
2026-01-22 19:47:04 -0500

    Hmw2
    Homework 2 Egpytian Historical Sites

---
+++ Homework2/sphinx.js
@@ -0,0 +1,6 @@
+var sphinx = {
+    "type": "FeatureCollection", "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } }, 
+    "features": [{ "type": "Feature", "properties": { "id": 1, "descrip": "Great Sphinx" }, 
+    "geometry": { "type": "Point", "coordinates": [31.1384, 29.97524] } }
+    ]
+    }
