3ec0ded629724e301b961bd7a5af753d3614efb6
Jesse Williams <jeswill@unc.edu>
2026-02-05 21:00:46 -0500

    Homework 3
    Finished up points of music venues

---
+++ Homework3/concert.js
@@ -0,0 +1,21 @@
+var concert = {
+    "type": "FeatureCollection","name": "Concert Venues", "features": 
+    [{ "type": "Feature", "properties": { "Venue": "Cat's Cradle", "Genre": "Any", "Address": "300 E Main St, Carrboro, NC 27510", "LAT":  35.9102211, "LON": -79.0683899}, "geometry": { "type": "Point", "coordinates": [-79.0683899,  35.9102211]}},
+    { "type": "Feature", "properties": { "Venue": "The Cave", "Genre": "Any", "Address": "452 1/2 W Franklin St, Chapel Hill, NC 27516", "LAT":  35.9102402, "LON": -79.0638428}, "geometry": { "type": "Point", "coordinates": [-79.0638428,  35.9102402]}},
+    { "type": "Feature", "properties": { "Venue": "Memorial Hall", "Genre": "Classical", "Address": "114 East Cameron Ave , Chapel Hill, NC 27514", "LAT": 35.9113992, "LON": -79.0521285}, "geometry": { "type": "Point", "coordinates": [-79.0521285,  35.9113992]}},
+    { "type": "Feature", "properties": { "Venue": "The Arts Center", "Genre": "Folk", "Address": "400 Roberson St, Carrboro, NC 27510", "LAT":  35.9095779, "LON": -79.0699702}, "geometry": { "type": "Point", "coordinates": [-79.0699702,  35.9095779]}},
+    { "type": "Feature", "properties": { "Venue": "Nightlight Bar & Club", "Genre": "EDM", "Address": "405 1/2 W Rosemary St, Chapel Hill, NC 27516", "LAT":  35.911425, "LON": -79.06264}, "geometry": { "type": "Point", "coordinates": [-79.06264,  35.911425]}},
+    { "type": "Feature", "properties": { "Venue": "Southern Village Music on The Green", "Genre": "Folk", "Address": "620 Market St, Chapel Hill, NC 27516", "LAT":  35.8810348, "LON": -79.0666232}, "geometry": { "type": "Point", "coordinates": [-79.0666232,  35.8810348]}},
+    { "type": "Feature", "properties": { "Venue": "Motorico Music Hall", "Genre": "Rock", "Address": "723 Rigsbee Ave, Durham, NC 27701", "LAT":  36.0035927, "LON": -78.9003640}, "geometry": { "type": "Point", "coordinates": [-78.9003640,  36.0035927]}},
+    { "type": "Feature", "properties": { "Venue": "Pinhook", "Genre": "Indie", "Address": "117 W Main St, Durham, NC 27701", "LAT":  35.9952545, "LON": -78.9014976}, "geometry": { "type": "Point", "coordinates": [-78.9014976,  35.9952545]}},
+    { "type": "Feature", "properties": { "Venue": "The Pitch", "Genre": "Any", "Address": "462 W Franklin St, Chapel Hill, NC 27516", "LAT":  35.91065, "LON": -79.062876}, "geometry": { "type": "Point", "coordinates": [-79.062876,  35.91065]}},
+    { "type": "Feature", "properties": { "Venue": "Lenovo Center", "Genre": "Any", "Address": "1400 Edwards Mill Road, Raleigh, NC 27601", "LAT":  35.803333, "LON": -78.721944}, "geometry": { "type": "Point", "coordinates": [-78.721944,  35.803333]}},
+    { "type": "Feature", "properties": { "Venue": "The Carolina Theatre", "Genre": "Any", "Address": "309 W Morgan St, Durham, NC 27701", "LAT":  35.99765, "LON": -78.90277}, "geometry": { "type": "Point", "coordinates": [-78.90277,  35.99765]}},
+    { "type": "Feature", "properties": { "Venue": "The Blue Note Grill", "Genre": "Jazz", "Address": "709 Washington St, Durham, NC 27701", "LAT":  36.00425, "LON": -78.90301}, "geometry": { "type": "Point", "coordinates": [-78.90301,  36.00425]}},
+    { "type": "Feature", "properties": { "Venue": "Duke Coffeehouse", "Genre": "Indie", "Address": "10 Epworth Dorm Ln, Durham, NC 27701", "LAT":  36.00670, "LON": -78.91331}, "geometry": { "type": "Point", "coordinates": [-78.91331,  36.00670]}},
+    { "type": "Feature", "properties": { "Venue": "Huron Stage Music Venue", "Genre": "Any", "Address": "2329 Huron St, Durham, NC 27707", "LAT":  35.98071, "LON": -78.92986}, "geometry": { "type": "Point", "coordinates": [-78.92986,  35.98071]}},
+    { "type": "Feature", "properties": { "Venue": "Rhythms Live Music Hall", "Genre": "Any", "Address": "2020 CHapel Hill Rd, Durham, NC 27707", "LAT":  35.98651, "LON": -78.92838}, "geometry": { "type": "Point", "coordinates": [-78.92838,  35.98651]}},
+    { "type": "Feature", "properties": { "Venue": "Local 506", "Genre": "Metal", "Address": "506 W Franklin St, Chapel Hill, NC 27516", "LAT":  35.9131, "LON": -79.0558}, "geometry": { "type": "Point", "coordinates": [-79.0558,  35.9131]}}
+        ]
+    }
+
